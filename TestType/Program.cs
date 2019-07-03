9e8ddd068788145b1b5baf9c6be0675aa8f5ae24
Юрий Молчанов <molchanov0726@mail.ru>
2019-07-03 21:16:26 +0300

    Fixed a lot of critical bugs. Now this project is having a little bug. I'm thinking about it. Trying to fix it.

--- TestType/Program.cs
+++ TestType/Program.cs
@@ -15,39 +15,51 @@ namespace TestType
         static void Main(string[] args)
         {
 
-            double a = 3, b = 4;
 
-           
+            double a = 0, b = 0;
 
             Console.WriteLine("Привет");
             Console.WriteLine("Как тебя зовут?");
-            string answer=Console.ReadLine();
-            Console.WriteLine("Добро пожаловать в мою таверну, "+answer) ;
+            string creator = Console.ReadLine();
+            if ("Юра" == creator || "Рома" == creator) {
+                Console.WriteLine("Добро пожаловать, создатель" + creator); }
+            else {
+                Console.WriteLine("Добро пожаловать в мою таверну, " + creator);
+            }
             Console.WriteLine("Что вы хотите выполнить?(Сложение(+),вычитание(-), умножение(*), деление(/).");
             string operation = Console.ReadLine();
-            
             Console.WriteLine("Введите первое число");
             string num = Console.ReadLine();
             double.TryParse(num, out a);
             Console.WriteLine("Введите второе число");
             num = Console.ReadLine();
             double.TryParse(num, out b);
-            if ("+" == operation) {
+            if ("+" == operation)
+            {
                 Console.WriteLine("Sum " + (a + b));
 
             }
-           else if ("-"== operation) { Console.WriteLine("Raz " + (a - b)); }
-          else  if ("*"==operation)
+            else if ("-" == operation)
+            {
+                Console.WriteLine("Raz " + (a - b));
+            }
+            else if ("*" == operation)
             {
                 Console.WriteLine("Umn " + (a * b));
-            }
-           else if ("/"==operation)
+            }      
+            else if ("/" == operation)
             {
+                if (b == 0) { Console.WriteLine("Del " + (a / b)); }
+                ;
                 Console.WriteLine("Del " + (a / b));
+            
+                Console.WriteLine("Операция невозможна");
             }
 
-            
-            Console.ReadLine(); 
+
+            Console.ReadLine();
         }
     }
 }
+// если ввожу имя Рома, или Юра- приветсвую создатель
+// если делим на 0 - вывести текст операция не выполняется
